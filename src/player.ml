7f63a7784c9abded8d22f4f8139786fad6882803
Humanest <44194343+humanest@users.noreply.github.com>
2023-12-06 11:19:59 -0500

    Tmp debug changes and test

--- src/player.ml
+++ src/player.ml
@@ -12,9 +12,30 @@ type t = {
   mutable sprite : int * int;
 }
 
-
 let player_speed = 1.0
-let player_sprite_num = 3
+let player_sprite_num = 4
+let string_of_player_state = function Alive -> "Alive" | Dead -> "Dead"
+
+let string_of_direction = function
+  | Up -> "Up"
+  | Down -> "Down"
+  | Left -> "Left"
+  | Right -> "Right"
+
+let string_of_player record =
+  let position_str =
+    Printf.sprintf "(%f, %f)" (fst record.position) (snd record.position)
+  in
+  let player_state_str = string_of_player_state record.player_state in
+  let move_counter_str = string_of_int record.move_counter in
+  let move_direction_str = string_of_direction record.move_direction in
+  let sprite_str =
+    Printf.sprintf "(%d, %d)" (fst record.sprite) (snd record.sprite)
+  in
+  Printf.sprintf
+    "{ position: %s; player_state: %s; move_counter: %s; move_direction: %s; \
+     sprite: %s }\n"
+    position_str player_state_str move_counter_str move_direction_str sprite_str
 
 let key_to_direction (key : key) : direction option =
   match key with
@@ -73,10 +94,11 @@ let update (player : t) (key : key) : t =
   in
   let new_pos = add_position player.position (direction_to_delta direction) in
   (match Game_map.get_location new_pos with
-  | Wall -> ()
+  | Wall -> print_endline (Printf.sprintf "Got wall at (%f, %f)" (fst new_pos) (snd new_pos));
   | _ -> player.position <- new_pos);
   player.move_counter <- (player.move_counter + 1) mod player_sprite_num;
   update_sprite player;
+  print_endline (string_of_player player);
   player
 
 let check_alive (player : t) : bool =
--- tests/tests.ml
+++ tests/tests.ml
@@ -1,19 +1,30 @@
 open Core
 open OUnit2
 open Enemy
+open Player
+
+(* Player Test *)
+let test_update_player _ =
+  assert_equal true @@ (Game_map.load "map.csv" && Render.load_sprite "sprite.png");
+  let player = Player.create (0.0, 0.0) in
+  let updated_player = Player.update player (Key 'c') in
+  assert_equal updated_player.move_counter @@ (player.move_counter + 1)
+
+let player_tests =
+  "Player Tests" >: test_list [ "test update player" >:: test_update_player ]
 
 (* Enemy Test *)
 (* let test_match_dir_to_ind () =
-  assert (match_dir_to_ind 0 = (1.0, 0.0));
-  assert (match_dir_to_ind 1 = (-1.0, 0.0));
-  assert (match_dir_to_ind 2 = (0.0, 1.0));
-  assert (match_dir_to_ind 3 = (0.0, -1.0));
-  try
-    let _ = match_dir_to_ind 5 in
-    assert false
-  with
-  | Failure "invalid int of direction" -> ()
-  | _ -> assert false *)
+   assert (match_dir_to_ind 0 = (1.0, 0.0));
+   assert (match_dir_to_ind 1 = (-1.0, 0.0));
+   assert (match_dir_to_ind 2 = (0.0, 1.0));
+   assert (match_dir_to_ind 3 = (0.0, -1.0));
+   try
+     let _ = match_dir_to_ind 5 in
+     assert false
+   with
+   | Failure "invalid int of direction" -> ()
+   | _ -> assert false *)
 
 let test_update_enemy _ =
   let test_enemy1 = Red_enemy.create (5.0, 5.0) in
@@ -21,16 +32,12 @@ let test_update_enemy _ =
   assert_equal 1 @@ updated_enemy.move_counter
 
 (* let test_new_game = new_game
-let test_game_win _ = 
-  test_new_game.state <- Win;
-  let test_game_next = update (Some 'w') test_new_game in
-  assert_equal 0 @@ test_game_next.score *)
-let tests =
-  "Pacman Tests"
-  >: test_list
-       [
-         "test distribution" >:: test_update_enemy;
-       ]
+   let test_game_win _ =
+     test_new_game.state <- Win;
+     let test_game_next = update (Some 'w') test_new_game in
+     assert_equal 0 @@ test_game_next.score *)
+let enemy_tests =
+  "Enemy Tests" >: test_list [ "test distribution" >:: test_update_enemy ]
 
-let series = "Pacman Tests" >::: [ tests ]
+let series = "Pacman Tests" >::: [ player_tests; enemy_tests ]
 let () = run_test_tt_main series
